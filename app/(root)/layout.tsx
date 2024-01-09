bed0ac47a38e2e42f703307df70155683198cb42
Onelio Viera <ojvwebdesign@gmail.com>
2024-01-08 21:26:00 -0700

    Add layout components and update page layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import Topbar from '@/components/shared/shared/Topbar'
+import LeftSidebar from '@/components/shared/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/shared/RightSidebar'
+import Bottombar from '@/components/shared/shared/Bottombar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +23,21 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Topbar />
+        <main>
+          <LeftSidebar />
+
+          <section className='main-content'>
+            <div className='w-full max-w-4xl'>
+              {children}
+            </div>
+          </section>
+
+          <RightSidebar />
+        </main>
+        <Bottombar />
+        </body>
     </html>
     </ClerkProvider>
   )
